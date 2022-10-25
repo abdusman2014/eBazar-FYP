76e64ba12e0e5c539e05f2b6247c7ed8c1c604ce
Usama.K98 <i190493@gmail.com>
2022-10-25 22:52:05 +0500

    added bottom sheet

---
+++ app/Components/FilterMenu.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, {useMemo, useRef} from 'react'
+
+import AppText from './AppText';
+
+export default function FilterMenu() {
+    
+  return (
+    <View>
+    
+    <AppText>Aloo</AppText>
+      
+    </View>
+  )
+}
+
+
+  
