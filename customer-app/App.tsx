52aa765a97f85a981817762a282efa250cfe8c7f
abdusman2014 <abdusman2014@gmail.com>
2023-07-12 00:59:47 +0500

    use push

--- customer-app/App.tsx
+++ customer-app/App.tsx
@@ -14,6 +14,7 @@ import firebase from "./firebase";
 import OrdersScreen from "./app/Screens/OrdersScreen";
 import TrackOrderScreen from "./app/Screens/TrackOrderScreen";
 import userStore from "./app/state-management/AppUser";
+//useless commtn
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
   const {user} = userStore();
